f04bb1572ab9b59d541c87ef961dfb04e94f038b
andriyash85 <andriyash85@gmail.com>
2022-02-08 16:02:51 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,28 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const numberRef = document.querySelector("input");
+const btnCrtRef = document.querySelector("button[data-create]");
+const btnDstRef = document.querySelector("button[data-destroy]");
+const boxRef = document.querySelector("#boxes");
+let arrCollection = [];
+const crtBtn = () => {
+  const number = numberRef.value;
+  for (let i = 0; i < number; i++) {
+    const divRef = document.createElement('div'); 
+ divRef.style.backgroundColor = getRandomHexColor();
+divRef.style.width = 30+10*i+"px";   
+    divRef.style.height = 30+10*i+"px"; 
+    arrCollection.push(divRef);  
+}
+  boxRef.append(...arrCollection);
+}
+const dstBtn = () => { 
+  arrCollection = [];
+  boxRef.innerHTML = '';
+  numberRef.value = '';
+
+};
+
+btnCrtRef.addEventListener("click", crtBtn);
+btnDstRef.addEventListener("click", dstBtn);
